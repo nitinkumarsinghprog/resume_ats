13f4e1caf9d659e56741bb148849ab7423e4c74c
Nitin Kumar <nitinsingh.kmr@gmail.com>
2026-07-16 16:55:51 +0530

    Added ScoreCircle in Resume filed

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import ScoreCircle from "./ScoreCircle";
 
 const ResumeCard = ({ resume }: { resume: Resume }) => {
   return (
@@ -7,13 +8,28 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
       className="resume-card animate-in fade-in duration-1000"
       to={"/resume/${resume.id}"}
     >
-      <div className="flex flex-col gap-2">
-        <h2 className="text-black font-bold break-words">
-          {resume.companyName}
-        </h2>
-        <h3 className="text-lg break-words text-gray-500">{resume.jobTitle}</h3>
+      <div className="resume-card-header">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-black font-bold wrap-break-word">
+            {resume.companyName}
+          </h2>
+          <h3 className="text-lg wrap-break-word text-gray-500">
+            {resume.jobTitle}
+          </h3>
+        </div>
+        <div className="flex-shrink-0">
+          <ScoreCircle score={resume.feedback.overallScore} />
+        </div>
       </div>
-      <div className="flex-shrink-0"></div>
+      <div className="gradient-border animate-in fade-in duration-1000">
+        <div className="w-full h-full">
+          <img
+            src={resume.imagePath}
+            alt="resume"
+            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+          />
+        </div>
+      </div>
     </Link>
   );
 };
