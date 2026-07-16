f556d76ddd16c93319a73b31fbfe85bb35cdd2f0
Nitin Kumar <nitinsingh.kmr@gmail.com>
2026-07-16 10:59:02 +0530

    Added components,constants and types

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">RESUME ATS</p>
+      </Link>
+      <Link to="/upload">
+        <p className="primary-button w-fit">Upload Resume</p>
+      </Link>
+    </nav>
+  );
+};
+export default Navbar;
